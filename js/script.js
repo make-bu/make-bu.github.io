0909384ac6a29ae1dd31735987a2a59e260a2030
jenny-s51 <jshandel12@gmail.com>
2018-10-18 22:32:40 -0400

    Changes to HTML and CSS, almost done

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 // Add scrollspy to <body>
 /*
   TODO Fix this
-$('body').scrollspy({target: ".navbar", offset: 50}); 
+$('body').scrollspy({target: ".navbar", offset: 50});
 
 $("#main-navbar a").on('click', function(e){
   if (this.hash !== "") {
     event.preventDefault();
     var hash = this.hash;
-    
+
     $('html, body').animate({
       scrollTop: $(hash).offset().top
     }, 800, function(){
@@ -16,24 +16,23 @@ $("#main-navbar a").on('click', function(e){
   }
 });
 */
-  
+
 //Scroll between the hero verbs
-var scrollHeroVerbs = ["Make","Hack","Create","Learn"];
+var scrollHeroVerbs = ["MAKE","HACK","CREATE","LEARN"];
 var iHero = 0; //incrementer for Hero text
 
 $(document).ready(function() {
-  
+
   $('#scroll-verbs').addClass("slide-up-down-animation");
-  
+
   function scrollHeroFn() {
     iHero = (iHero >= scrollHeroVerbs.length-1) ? 0 : iHero+1;
     $('#scroll-verbs').text(function(i,t){
       return scrollHeroVerbs[iHero];
     });
   }
-  
+
   scrollHeroFn(); //Run once
   setInterval(function(){scrollHeroFn()}, 2000); //then repeat
-  
-});
 
+});
